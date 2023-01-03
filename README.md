0abf50294a88e0097207a42eb0c17aebf2aa3e4e
Sarah L <slaukilo@gmail.com>
2023-01-02 17:08:31 -0700

    Add README and standard CLI call information

---
+++ README.md
@@ -0,0 +1,18 @@
+<div align="center">
+
+# `RUSTish`
+
+<h3>
+    A lightweight fetch system written in <code>Rust⚙️</code>
+</h3>
+<br>
+
+</div>
+
+# Description
+
+`rustish` is a an incredibll fast fetch system written completely in `Rust`.
+
+
+# Usage
+still to come...
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use clap::{Arg, ArgMatches, Command};
+use std::process;
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("rtch")
+        .version("1.0")
+        .author("Mortiferis <me@email.com>")
+        .about("rtch is a lightweight and simple fetch system built \
+                in Rust!")
+        .after_help("Longer explanation to appear after the options when \
+                displaying the help information from --help or -h")
+        .display_name("rtch")
+        .get_matches();
+   
 }
